7f882aed245f95c8f39b7a561a7ab875cf19c4fa
Puyan Wei <puyan.wei@mountainwarehouse.com>
2018-10-03 17:32:49 +0100

    Setup routing for '/' and tested endpoint

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const router = require('./router');
 
 const app = express();
 
 // App setup
 app.use(morgan('combined')); //console logging framework middleware
 app.use(bodyParser.json({ type: '*/*' })); //parses incoming requests into json middleware
+router(app);
 
 // Server setup
 const port = process.env.PORT || 3090;
