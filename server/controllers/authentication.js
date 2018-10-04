abb21d8fc525f7e0b92031308ae58f9610c68ae1
Puyan Wei <puyan.wei@mountainwarehouse.com>
2018-10-04 17:31:41 +0100

    Added encryption config for passwords

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -4,6 +4,11 @@ exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
+    // Both fields must have an entry to be valid
+    if (!email || !password) {
+        return errorStatus('You must provide email and password');
+    }
+
     // Does email exist?
     User.findOne({ email: email }, function(err, existingUser) {
         if (err) {
@@ -11,7 +16,7 @@ exports.signup = function(req, res, next) {
         }
         // Yes => Return an Error
         if (existingUser) {
-            return res.status(422).send({ error: 'Email is in use' }); // 422 is 'unprocessable entity'
+            return errorStatus('Email is in use');
         }
         // No => Create and save user record
         const user = new User({
@@ -26,4 +31,8 @@ exports.signup = function(req, res, next) {
         // Respond with success message
         res.json({ success: true });
     });
+
+    function errorStatus(message) {
+        res.status(422).send({ error: message }); // 422 is 'unprocessable entity'
+    }
 };
--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,32 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 const Schema = mongoose.Schema;
 
 // Define our model
 const userSchema = new Schema({
     email: { type: String, lowercase: true, unique: true },
     password: String
+});
+
+// On save hook, encrypt password
+userSchema.pre('save', function(next) {
+    // Generate a salt then run callback
+    const user = this;
+    // Generate a salt
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) {
+            return next(err);
+        }
+        // Encrypt our password using salt
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err) {
+                return next(err);
+            }
+            // Overwrite plain text password with encrypted password
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 // Create the model class
